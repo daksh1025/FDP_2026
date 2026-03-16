47b624e6a5313198f52ff0c89db73038222b5e79
daksh1025 <kansaradax1025@gmail.com>
2026-03-17 00:27:10 +0530

    Delete Portfolio directory

--- Portfolio/script.js
+++
@@ -1,7 +0,0 @@
-// Navbar background change on scroll
-window.addEventListener("scroll", function () {
-    const navbar = document.querySelector(".navbar");
-    navbar.style.background = window.scrollY > 50 
-        ? "#0f172a" 
-        : "rgba(15,23,42,0.9)";
-});
